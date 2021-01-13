0e4c1f038e34aa767d10afa16e0470581f280e61
ClassyBrute <58109973+ClassyBrute@users.noreply.github.com>
2021-01-13 13:58:22 +0100

    Add files via upload

--- lista5/zad3.py
+++ lista5/zad3.py
@@ -1,19 +1,21 @@
 import numpy as np
-from scipy.interpolate import splev
-from scipy.interpolate import splrep
+from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
-from scipy.optimize import bisect
-from scipy.misc import derivative
 
 Re = np.array([0.2, 2, 20, 200, 2000, 20000])
 Cd = np.array([103, 13.9, 2.72, 0.8, 0.401, 0.433])
 
-tck = splrep(Re, Cd, s=0)
+dane = CubicSpline(np.log(Re), np.log(Cd))
 
-Re_new = np.arange(0.2, 20000, 0.1)
-Cd_new = splev(Re_new, tck, der=0)
+x = np.array([5, 50, 5000])
+y = np.exp(dane(np.log(x)))
 
-plt.figure()
-plt.loglog(Re, Cd, 'o', Re_new, Cd_new, '-')
+x1 = np.arange(0.2, 20000, 1)
+y1 = np.exp(dane(np.log(x1)))
 
+plt.plot(Re, Cd, 'o')
+plt.plot(x, y, 'ob')
+plt.plot(x1, y1)
+plt.xscale('log')
+plt.yscale('log')
 plt.show()
