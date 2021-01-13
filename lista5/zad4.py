e1c3eaa57ddf249d6abf11ca0a128edbf68d357f
ClassyBrute <58109973+ClassyBrute@users.noreply.github.com>
2021-01-13 14:15:17 +0100

    Add files via upload

---
+++ lista5/zad4.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.interpolate import lagrange
+import matplotlib.pyplot as plt
+
+Re = np.array([0.2, 2, 20, 200, 2000, 20000])
+Cd = np.array([103, 13.9, 2.72, 0.8, 0.401, 0.433])
+
+dane = lagrange(np.log(Re), np.log(Cd))
+
+x = np.array([5, 50, 5000])
+y = np.exp(dane(np.log(x)))
+
+x1 = np.arange(0.2, 20000, 1)
+y1 = np.exp(dane(np.log(x1)))
+
+plt.plot(Re, Cd, 'o')
+plt.plot(x, y, 'ob')
+plt.plot(x1, y1)
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
